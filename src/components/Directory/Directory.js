400dcd4b4daef590e8a2cd4dcdc125d7520d7a98
Roman <mamontov.r1995@gmail.com>
2021-07-27 19:26:48 +0300

    update menu-items to navigate with router HOC

--- src/components/Directory/Directory.js
+++ src/components/Directory/Directory.js
@@ -11,29 +11,34 @@ class Directory extends Component {
         {
           title: 'hats',
           imageUrl: 'https://i.pinimg.com/originals/4b/9a/ac/4b9aac6da2819e8c97154da6134c0899.jpg',
-          id: 1
+          id: 1,
+          linkUrl: 'hats'
         },
         {
           title: 'jackets',
           imageUrl: 'https://i.ebayimg.com/00/s/NDQ0WDUwMA==/z/1Y0AAOSwhYNeR87I/$_57.JPG?set_id=8800005007',
-          id: 2
+          id: 2,
+          linkUrl: ''
         },
         {
           title: 'sneakers',
           imageUrl: 'https://sporteluxe-wp.s3-ap-southeast-2.amazonaws.com/wp-content/uploads/2014/06/Converse-post-2.jpg',
-          id: 3
+          id: 3,
+          linkUrl: ''
         },
         {
           title: 'mans',
           imageUrl: 'https://ups.aopcdn.com/s23737/goods/18235/0u3d6ee632fa0a485cbe520af2e8647ed5.jpg',
           id: 4,
-          size: 'large'
+          size: 'large',
+          linkUrl: ''
         },
         {
           title: 'women',
           imageUrl: 'http://g01.a.alicdn.com/kf/HL1deH.FxBjXXagOFbXz/202312194/HL1deH.FxBjXXagOFbXz.jpg?size=55458&amp;height=700&amp;width=700&amp;hash=25ca3b423d5cc97cbe16e52525b6d3bc',
           id: 5,
-          size: 'large'
+          size: 'large',
+          linkUrl: ''
         }
       ]
     }
@@ -49,6 +54,7 @@ class Directory extends Component {
              title={section.title}
              imageUrl={section.imageUrl}
              size={section.size}
+             linkUrl={section.linkUrl}
             />
           })
         }
--- src/components/MenuItem/MenuItem.js
+++ src/components/MenuItem/MenuItem.js
@@ -1,16 +1,18 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 
 import './MenuItem.scss';
 
-const MenuItem = ({ title, imageUrl, size }) => {
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match  }) => {
   return (
-    <div className={`${size} menu-item`} style={{ backgroundImage: `url(${imageUrl})` }}>
+    <div className={`${size} menu-item`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
+      <div className='background-image' style={{ backgroundImage: `url(${imageUrl})` }}></div>
       <div className='content'>
-        <h2 className='title'>{title}</h2>
+        <h2 className='title'>{title.toUpperCase()}</h2>
         <p className='subtitle'>SHOP NOW</p>
       </div>
     </div>
   );
 }
 
-export default MenuItem;
+export default withRouter(MenuItem);
