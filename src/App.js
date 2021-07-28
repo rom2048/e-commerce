5f8e46a2ae048f19accb977d65207ecd085b01db
Roman <mamontov.r1995@gmail.com>
2021-07-28 12:03:26 +0300

    add header component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import HomePage from './pages/HomePage/HomePage';
 import ShopPage from './pages/Shop/Shop';
 import { Route, Switch } from 'react-router-dom';
+import Header from './components/Header/Header';
 import './App.css';
 
 function App() {
   return (
     <div >
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route  path='/shop' component={ShopPage} />
---
+++ src/components/Header/Header.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { ReactComponent as Logo } from '../../assets/crown.svg';
+import './Header.scss';
+
+const Header = () => {
+  return (
+    <header className='header'>
+      <Link className='logo-container' to='/'>
+        <Logo className='logo' />
+      </Link>
+      <div className='options'>
+        <Link className='option f4 fw6 purple no-underline underline-hover' to='/shop'>
+          SHOP
+        </Link>
+        <Link className='option f4 fw6 purple no-underline underline-hover' to='/shop'>
+          КОНТАКТЫ
+        </Link>
+        <Link className='option f4 fw6 purple no-underline underline-hover' to='/shop'>
+          About us 
+        </Link>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
