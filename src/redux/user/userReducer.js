df22e704ca79ea55a62087fd0434f812fc492a92
Roman <mamontov.r1995@gmail.com>
2021-08-14 12:53:57 +0300

    update user sign out with sagas

--- src/redux/user/userReducer.js
+++ src/redux/user/userReducer.js
@@ -1,6 +1,8 @@
 import {
   SIGN_IN_SUCCESS,
   SIGN_IN_FAILED,
+  SIGN_OUT_SUCCESS,
+  SIGN_OUT_FAILED
 } from '../constants.js';
 
 const initialStateUser = {
@@ -12,8 +14,11 @@ const userReducer = (state=initialStateUser, action={}) => {
   switch(action.type){
     case SIGN_IN_SUCCESS:
       return Object.assign({}, state, {currentUser: action.payload, error: null});
+    case SIGN_OUT_SUCCESS:
+      return Object.assign({}, state, {currentUser: null, error: null});
     case SIGN_IN_FAILED:
-      return Object.assign({}, state, {error: action.payload })
+    case SIGN_OUT_FAILED:
+      return Object.assign({}, state, {error: action.payload });
     default:
       return state;
   }
